fd57a3b35236f00328cbc7675f6e36c868329911
phunguyenTPP <phunguyen713@gmail.com>
2024-01-03 09:27:43 +0700

    update utils & alembic

--- frontend/src/graphql/utils.ts
+++ frontend/src/graphql/utils.ts
@@ -1,15 +1,33 @@
 type PrimaryValue = number | string | boolean | null | undefined;
 
-export type Query<T extends Record<string, any>> = {
+const QUERY_VARIABLES_KEY = "variables";
+const QUERY_FIELDS_KEY = "fields";
+
+type ExtendedType<T> = {
+  [K in keyof T]: T[K] | { value: T[K]; isEnum: boolean };
+};
+
+export type QueryBuilder<T extends Record<string, any>> = {
   [K in keyof T]?: T[K] extends PrimaryValue | PrimaryValue[]
-    ? boolean
+    ? true
     : T[K] extends (args: infer A) => any
-    ? (args: A) => Query<ReturnType<T[K]>> & { fnArgs: A }
+    ? { [QUERY_FIELDS_KEY]: QueryBuilder<ReturnType<T[K]>> } & {
+        [QUERY_VARIABLES_KEY]: ExtendedType<A>;
+      }
     : T[K] extends Record<string, any>[] | PrimaryValue
-    ? Query<Extract<T[K], Record<string, any>[]>[0]>
+    ? {
+        [QUERY_FIELDS_KEY]: QueryBuilder<
+          Extract<T[K], Record<string, any>[]>[0]
+        >;
+      }
     : T[K] extends Record<string, any> | PrimaryValue
-    ? Query<Extract<T[K], Record<string, any>>>
+    ? { [QUERY_FIELDS_KEY]: QueryBuilder<Extract<T[K], Record<string, any>>> }
     : never;
+};
+
+type Query<T extends Record<string, any>> = { query: QueryBuilder<T> };
+type Mutation<T extends Record<string, any>> = {
+  mutation: QueryBuilder<T>;
 };
 
 const tab = (level: number) => Array(level).fill("  ").join("");
@@ -40,20 +58,26 @@ const queryKeyToString = (queries: Record<string, any>, level = 0) => {
   let result = "";
   for (const queryKey in queries) {
     let loopLevel = level;
-    if (queryKey === "fnArgs") {
+    if (queryKey === QUERY_VARIABLES_KEY) {
       continue;
-    }
-    if (typeof queries[queryKey] === "object") {
+    } else if (queryKey === QUERY_FIELDS_KEY) {
+      const inside = queryKeyToString(queries[queryKey], loopLevel + 1);
+      const thisTab = tab(loopLevel);
+      result += `${thisTab}${inside}`;
+    } else if (
+      typeof queries[queryKey] === "object" &&
+      QUERY_VARIABLES_KEY in queries[queryKey]
+    ) {
+      const inside = queryKeyToString(queries[queryKey], loopLevel + 1);
+      const argsStr = fnArgsToString(queries[queryKey][QUERY_VARIABLES_KEY]);
+      const thisTab = tab(loopLevel);
+
+      result += `\n${thisTab}${queryKey}(${argsStr}) {${inside}\n${thisTab}}`;
+    } else if (typeof queries[queryKey] === "object") {
       const inside = queryKeyToString(queries[queryKey], loopLevel + 1);
       const thisTab = tab(loopLevel);
 
       result += `\n${thisTab}${queryKey} {${inside}\n${thisTab}}`;
-    } else if (typeof queries[queryKey] === "function") {
-      const inside = queryKeyToString(queries[queryKey](), loopLevel + 1);
-      const argsStr = fnArgsToString(queries[queryKey]().fnArgs);
-      const thisTab = tab(loopLevel);
-
-      result += `\n${thisTab}${queryKey}(${argsStr}) {${inside}\n${thisTab}}`;
     } else if (queries[queryKey]) {
       const thisTab = tab(loopLevel);
 
@@ -63,14 +87,12 @@ const queryKeyToString = (queries: Record<string, any>, level = 0) => {
   return result;
 };
 
-export const queryBuilder = <T extends Record<string, any>>(query: {
-  query: Query<T>;
-}) => {
+const queryBuilder = <T extends Record<string, any>>(query: Query<T>) => {
   return queryKeyToString(query);
 };
 
-export const mutationBuilder = <T extends Record<string, any>>(query: {
-  mutation: Query<T>;
-}) => {
-  return queryKeyToString(query);
+const mutationBuilder = <T extends Record<string, any>>(
+  mutation: Mutation<T>
+) => {
+  return queryKeyToString(mutation);
 };
