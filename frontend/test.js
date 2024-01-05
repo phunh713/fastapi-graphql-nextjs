b6f30bdad44a99b2f9bd7b49438eb736b191032a
phunguyenTPP <phunguyen713@gmail.com>
2024-01-05 17:29:49 +0700

    temp commit

--- frontend/test.js
+++ frontend/test.js
@@ -8,15 +8,28 @@ const data = fs.readFileSync("./src/graphql/strapiGeneratedTypes.ts", {
 const lines = data.split("\n");
 
 let start = false;
+let paramsType = "";
 
 lines.map((l) => {
   if (!start && l.includes("type Query =")) {
+    paramsType = "Query";
+    start = true;
+    return l;
+  }
+
+  if (!start && l.includes("type Mutation =")) {
+    paramsType = "Mutation";
     start = true;
     return l;
   }
 
   if (start) {
     const [arg, value] = l.split(":");
-    return;
+    const params = `(${paramsType}${
+      arg.endsWith("?") ? arg.slice(0, -1) : arg
+    })`;
+    return `${arg}: ${params} => ${value}`;
   }
 });
+
+console.log(lines.join("\n"));
