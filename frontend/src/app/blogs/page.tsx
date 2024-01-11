cbbb46593b5c3e72cd1fa783f90c09424b3d439b
phunguyenTPP <phunguyen713@gmail.com>
2024-01-11 16:14:22 +0700

    add revalidate 600

--- frontend/src/app/blogs/page.tsx
+++ frontend/src/app/blogs/page.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { strapiQueryClient } from "@/config/strapi";
 
-export const revalidate = 0;
+export const revalidate = 600;
 
 const BlogsPage = async () => {
   const strapiData = await strapiQueryClient({
